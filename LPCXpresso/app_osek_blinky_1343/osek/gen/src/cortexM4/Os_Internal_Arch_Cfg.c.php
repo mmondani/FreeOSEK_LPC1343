0791e8862020ab3a927fbeab16018c247b215157
mmondani <mmondani89@gmail.com>
2016-04-16 19:53:58 -0300

    * Se modifica el proyecto app_osek_blinky_1343 para hacerlo compatible con el LPC1343. La aplicación funciona. * Se compilan con el mayor nivel de optimización (-O3) los proyectos lpc_board_nxp_lpcxpresso_1343 y lpc_chip_13xx para reducir el footprint en los demás proyectos. * Se modifica el .gitignore de la carpeta raiz. Queda vacío. * Se agrega un .gitignore en la carpeta LPCXpresso el cual tiene los elementos a ignorar para el tipo de proyectos de LPCXpresso. De esta forma puede haber un .gitignore en otras carpetas, para el tipo de elementos a ignorar para esos proyectos.

--- LPCXpresso/app_osek_blinky_1343/osek/gen/src/cortexM4/Os_Internal_Arch_Cfg.c.php
+++ LPCXpresso/app_osek_blinky_1343/osek/gen/src/cortexM4/Os_Internal_Arch_Cfg.c.php
@@ -68,6 +68,8 @@
 
 /*==================[macros and definitions]=================================*/
 
+#define WEAK __attribute__ ((weak))
+
 /*==================[internal data declaration]==============================*/
 
 /*==================[internal functions declaration]=========================*/
@@ -81,6 +83,9 @@ extern void ResetISR(void);
 
 /** \brief External declaration for the pointer to the stack top from the Linker Script */
 extern void _vStackTop(void);
+
+/** \brief External declaration for LPC MCU vector table checksum from  Linker Script */
+WEAK extern void __valid_user_code_checksum();
 
 /** \brief Handlers used by OSEK */
 extern void SysTick_Handler(void);
@@ -132,41 +137,64 @@ void DebugMon_Handler(void) {
  * See lpc_chip_175x_6x/inc/cmsis_175x_6x.h.
  */
 $intList = array (
-	0 => "WDT",
-	1 => "TIMER0",
-	2 => "TIMER1",
-	3 => "TIMER2",
-	4 => "TIMER3",
-	5 => "UART0",
-	6 => "UART1",
-	7 => "UART2",
-	8 => "UART3",
-	9 => "PWM1",
-	10 => "I2C0",
-	11 => "I2C1",
-	12 => "I2C2",
-	13 => "SPI",
-	14 => "SSP0",
-	15 => "SSP1",
-	16 => "PLL0",
-	17 => "RTC",
-	18 => "EINT0",
-	19 => "EINT1",
-	20 => "EINT2",
-	21 => "EINT3",
-	22 => "ADC",
-	23 => "BOD",
-	24 => "USB",
-	25 => "CAN",
-	26 => "DMA",
-	27 => "I2S",
-	28 => "ETH",
-	29 => "RIT",
-	30 => "MCPWM",
-	31 => "QEI",
-	32 => "PLL1",
-	33 => "USBAct",
-	34 => "CANAct"
+  0 => "WAKEUP",
+  1 => "WAKEUP",
+  2 => "WAKEUP",
+  3 => "WAKEUP",
+  4 => "WAKEUP",
+  5 => "WAKEUP",
+  6 => "WAKEUP",
+  7 => "WAKEUP",
+  8 => "WAKEUP",
+  9 => "WAKEUP",
+  10 => "WAKEUP",
+  11 => "WAKEUP",
+  12 => "WAKEUP",
+  13 => "WAKEUP",
+  14 => "WAKEUP",
+  15 => "WAKEUP",
+  16 => "WAKEUP",
+  17 => "WAKEUP",
+  18 => "WAKEUP",
+  19 => "WAKEUP",
+  20 => "WAKEUP",
+  21 => "WAKEUP",
+  22 => "WAKEUP",
+  23 => "WAKEUP",
+  24 => "WAKEUP",
+  25 => "WAKEUP",
+  26 => "WAKEUP",
+  27 => "WAKEUP",
+  28 => "WAKEUP",
+  29 => "WAKEUP",
+  30 => "WAKEUP",
+  31 => "WAKEUP",
+  32 => "WAKEUP",
+  33 => "WAKEUP",
+  34 => "WAKEUP",
+  35 => "WAKEUP",
+  36 => "WAKEUP",
+  37 => "WAKEUP",
+  38 => "WAKEUP",
+  39 => "WAKEUP",
+	40 => "I2C",
+	41 => "TIMER16_0",
+	42 => "TIMER16_1",
+	43 => "TIMER32_0",
+	44 => "TIMER32_1",
+	45 => "SSP0",
+	46 => "UART",
+	47 => "USB",
+  48 => "USB_FIQ",
+  49 => "ADC",
+	50 => "WDT",
+	51 => "BOD",
+	52 => "FMC",
+	53 => "PIOINT3",
+	54 => "PIOINT2",
+	55 => "PIOINT1",
+	56 => "PIOINT0",
+	57 => "SSP1"
 );
 
 $MAX_INT_COUNT = max(array_keys($intList))+1;
@@ -182,7 +210,7 @@ void (* const g_pfnVectors[])(void) = {
    MemManage_Handler,              /* The MPU fault handler      */
    BusFault_Handler,               /* The bus fault handler      */
    UsageFault_Handler,             /* The usage fault handler    */
-   0,                              /* Reserved                   */
+   __valid_user_code_checksum,     /* Reserved                   */
    0,                              /* Reserved                   */
    0,                              /* Reserved                   */
    0,                              /* Reserved                   */
--- LPCXpresso/app_osek_blinky_1343/src/app_osek_blinky_1343.c
+++ LPCXpresso/app_osek_blinky_1343/src/app_osek_blinky_1343.c
@@ -16,33 +16,126 @@
 #endif
 #endif
 
+#include <stdio.h>
 #include <cr_section_macros.h>
 
-// TODO: insert other include files here
+#include "os.h"               /* <= operating system header */
 
-// TODO: insert other definitions and declarations here
+/*
+ * See the OIL configuration file in
+ * etc/config.oil
+ */
 
-int main(void) {
+/* Comment this line in order to use LPC-Link based printf()
+ * If uncommented, all printf() calls will be disabled
+ */
+#define printf(...)
 
+int main(void)
+{
 #if defined (__USE_LPCOPEN)
+#if !defined(NO_BOARD_LIB)
     // Read clock settings and update SystemCoreClock variable
     SystemCoreClockUpdate();
-#if !defined(NO_BOARD_LIB)
     // Set up and initialize all required blocks and
     // functions related to the board hardware
     Board_Init();
     // Set the LED to the state of "On"
-    Board_LED_Set(0, true);
+    Board_LED_Set(0, false);
 #endif
 #endif
 
-    // TODO: insert code here
+    printf("Starting OSEK-OS in AppMode1\n");
+    StartOS(AppMode1);
 
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
-    while(1) {
-        i++ ;
-    }
+    /* we shouldn't return here */
+    while(1);
+
     return 0 ;
+}
+
+
+void ErrorHook(void)
+{
+	/* kernel panic :( */
+	ShutdownOS(0);
+}
+
+TASK(TaskInit)
+{
+	/* Set 500 tick alarm for TaskPeriodic */
+	printf("InitTask: SetRelAlarm for TaskPeriodic.\n");
+	SetRelAlarm(ActivateTaskPeriodic, 0, 500);
+
+	/* Set 1000 tick callback alarm */
+	printf("InitTask: SetRelAlarm for AppCallback.\n");
+	SetRelAlarm(AppCallbackAlarm, 100, 1000);
+
+	/* Activate TaskBlink */
+	printf("InitTask: Activate TaskBlink.\n");
+	ActivateTask(TaskBlink);
+
+	/* Activate TaskBackground */
+	ActivateTask(TaskBackground);
+
+	/* end InitTask */
+	printf("InitTask: TerminateTask().\n");
+	TerminateTask();
+}
+
+/*
+ * This task waits for an event to be set in order
+ * to continue execution.
+ */
+TASK(TaskBlink)
+{
+	printf("TaskBlink: Init.\n");
+	while(1)
+	{
+		printf("TaskBlink: Waiting for event...\n");
+		WaitEvent(evBlink);
+		ClearEvent(evBlink);
+		printf("TaskBlink: LED Toggle.\n");
+		Board_LED_Toggle(0);
+	}
+	TerminateTask();
+}
+
+/*
+ * This is a periodic task.
+ */
+TASK(TaskPeriodic)
+{
+	printf("TaskPeriodic: Event set.\n");
+	SetEvent(TaskBlink, evBlink);
+
+	/* end TaskPeriodic */
+	TerminateTask();
+}
+
+/*
+ * Just a background task with an infinite loop,
+ * it has to be defined with the minimum priority!!!
+ */
+TASK(TaskBackground)
+{
+	volatile int i = 0;
+	printf("TaskBackground: Running!\n");
+	while(1)
+	{
+		i++;
+		if(i == 0xFFFFF)
+		{
+			printf("TaskBackground still running...\n");
+			i = 0;
+		}
+	}
+}
+
+/*
+ * Alarm Callback example.
+ */
+ALARMCALLBACK(AppCallback)
+{
+	printf("AppCallback.\n");
 }
